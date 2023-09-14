c75f5f56c54ee83e277d86386ef2b91c789157db
Ahmed Osama <ao198523@gmail.com>
2023-09-14 03:08:54 +0300

    initial project. add splash screen. add empty home screen.

---
+++ lib/home_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeSccreen extends StatelessWidget {
+  const HomeSccreen({super.key});
+  static const routeName = 'route_name';
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
